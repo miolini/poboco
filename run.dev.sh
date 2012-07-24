9fa0ac095c607da77d8b2bfed15afa1fa7c48ab3
miolini <miolini@gmail.com>
2012-07-25 02:24:40 +0400

    добавлены скрипты запуска

---
+++ run.dev.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+HOST=localhost PORT=3050 supervisor -p 500 app.js
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-NODE_ENV=production PORT=3050 supervisor -p 500 app.js
+NODE_ENV=production HOST=localhost PORT=3050 supervisor -p 500 app.js
